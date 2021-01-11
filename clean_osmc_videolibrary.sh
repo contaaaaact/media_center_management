cba66d73460f743659c1cc88d7b1be9f49b110ed
contaaaaact <50547637+contaaaaact@users.noreply.github.com>
2021-01-11 09:46:46 +0100

    Add files via upload

--- clean_osmc_videolibrary.sh
+++ clean_osmc_videolibrary.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 #nettoyage de la médiathèque osmc
 
+if [ -d "/mnt/osmc/pegasus1/films" ]
+	then
+		echo -e "Pegasus1 est monté.";
+	else
+		echo -e "\nATTENTION pegasus1 n'est pas monté, le script va quitter" && sleep 1d && exit 0;
+fi
+
+if [ -d "/mnt/osmc/pegasus2/films" ]
+	then
+		echo -e "Pegasus2 est monté.";
+	else
+		echo -e "\nATTENTION pegasus2 n'est pas monté, le script va quitter" && sleep 1d && exit 0;
+fi
+				
+if [ -d "/mnt/osmc/pegasus3/films" ]
+	then
+		echo -e "Pegasus3 est monté.";
+	else
+		echo -e "\nATTENTION pegasus3 n'est pas monté, le script va quitter" && sleep 1d && exit 0;
+fi		
+		
+if [ -d "/mnt/osmc/pegasus4/films" ]
+	then
+		echo -e "Pegasus4 est monté.";
+	else
+		echo -e "\nATTENTION pegasus4 n'est pas monté, le script va quitter" && sleep 1d && exit 0;
+fi
+
 
 ssh osmc@192.168.0.21 'xbmc-send --action="CleanLibrary(video)"' && echo -e "\nNettoyage de la médiathèque effectué" && sleep 5
 
