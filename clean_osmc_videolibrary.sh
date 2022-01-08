31adbf859179219f8ec4c6c43b96646ded1402b5
contaaaaact <50547637+contaaaaact@users.noreply.github.com>
2022-01-08 15:57:40 +0100

    Add files via upload

--- clean_osmc_videolibrary.sh
+++ clean_osmc_videolibrary.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 #nettoyage de la médiathèque osmc
 
-if [ -d "/mnt/osmc/pegasus1/films" ]
+if [ -f "/mnt/osmc/pegasus1/check_pegasus1.txt" ]
 	then
 		echo -e "Pegasus1 est monté.";
 	else
 		echo -e "\nATTENTION pegasus1 n'est pas monté, le script va quitter" && sleep 1d && exit 0;
 fi
 
-if [ -d "/mnt/osmc/pegasus2/films" ]
+if [ -f "/mnt/osmc/pegasus2/check_pegasus2.txt" ]
 	then
 		echo -e "Pegasus2 est monté.";
 	else
 		echo -e "\nATTENTION pegasus2 n'est pas monté, le script va quitter" && sleep 1d && exit 0;
 fi
 				
-if [ -d "/mnt/osmc/pegasus3/films" ]
+if [ -f "/mnt/osmc/pegasus3/check_pegasus3.txt" ]
 	then
 		echo -e "Pegasus3 est monté.";
 	else
 		echo -e "\nATTENTION pegasus3 n'est pas monté, le script va quitter" && sleep 1d && exit 0;
 fi		
 		
-if [ -d "/mnt/osmc/pegasus4/films" ]
+if [ -f "/mnt/osmc/pegasus4/check_pegasus4.txt" ]
 	then
 		echo -e "Pegasus4 est monté.";
 	else
